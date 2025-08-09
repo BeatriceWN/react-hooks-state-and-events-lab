003c8857940fcb1f710a0349918dea287c34593d
Beatrice Wambui <bettyneema2@gmail.com>
2025-08-09 13:24:18 +0300

    initial commit state and events lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false); //Create a state variable for dark mode
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  function handleDarkModeClick() {
+    setIsDarkMode((prevMode) => !prevMode); // Event handler to toggle dark mode
+  }
+
+  const appClass = isDarkMode ? "App dark" : "App light"; // Decide which CSS class to use based on state
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkModeClick}> {/* Button text changes based on mode */}
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
